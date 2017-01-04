935a1ae6835f78262c033a7afeeb8145634e4030
Иван Чистяков <efh996@gmail.com>
2017-01-04 23:29:20 +0500

    Initial Commit

---
+++ WallpaperMaster/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WallpaperMaster
+//
+//  Created by Ivan Chistyakov on 04.01.17.
+//  Copyright © 2017 Ivan Chistyakov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WallpaperMaster/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WallpaperMaster
+//
+//  Created by Ivan Chistyakov on 04.01.17.
+//  Copyright © 2017 Ivan Chistyakov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
