7be641955f0c49c9fd81c705323ce61086817623
Иван Чистяков <efh996@gmail.com>
2017-01-06 01:01:49 +0500

    NatGeo photo collection expanded.

--- WallpaperMaster/DesktopUpdater.swift
+++ WallpaperMaster/DesktopUpdater.swift
@@ -14,26 +14,34 @@ class DesktopUpdater {
     var imageGetter: ImageGetterDelegate? = nil
     
     init() {
-        // создаём папку для хранения картинок
+        // create folder for the application where all the wallpapers will be saved
         let directory    = FileManager.SearchPathDirectory.documentDirectory
         let mask         = FileManager.SearchPathDomainMask.userDomainMask
         let paths        = NSSearchPathForDirectoriesInDomains(directory, mask, true)
         let documentsDir = paths.first
-        appFolder        = URL(fileURLWithPath: documentsDir!).appendingPathComponent("WallpaperMaster")
-
-        let FMDefault = FileManager.default
+        self.appFolder   = URL(fileURLWithPath: documentsDir!).appendingPathComponent("WallpaperMaster")
+        let FMDefault    = FileManager.default
         try? FMDefault.createDirectory(at: appFolder, withIntermediateDirectories: false, attributes: [:])
 
+        self.imageGetter = NatGeoCollection()
+        
+        // update wallpaper immediately after launch
+        self.updateWallpaper()
+        
+        // launch timer to update wallpapers automatically
         Timer.scheduledTimer(timeInterval: 30, target: self, selector: #selector(updateWallpaper), userInfo: nil, repeats: true)
     }
     
     @objc func updateWallpaper() {
+        // download new wallpaper
         let newImage = self.imageGetter?.getRandomImage()
         
+        // create random name for the image and save it
         let num = arc4random()
         let imageURL = appFolder.appendingPathComponent("\(num).jpg")
         newImage?.savePNG(imageURL.path)
         
+        // set image as desktop wallpaper
         do {
             let workspace = NSWorkspace.shared()
             let screen = NSScreen.main()!
