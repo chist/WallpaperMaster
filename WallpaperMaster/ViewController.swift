09ddcbe76712ccbea8b2bd34e194518469de3c88
Иван Чистяков <efh996@gmail.com>
2017-01-09 03:03:24 +0500

    Build number in Xcode and in app window changes automatically.

--- WallpaperMaster/ViewController.swift
+++ WallpaperMaster/ViewController.swift
@@ -9,15 +9,24 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet var versionLabel: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do view setup here.
+        
+        versionLabel.stringValue = self.getAppVersion()
     }
     
     override func viewDidAppear() {
         super.viewDidAppear()
         NSApp.activate(ignoringOtherApps: true)
+    }
+    
+    func getAppVersion() -> String {
+        let dictionary = Bundle.main.infoDictionary!
+        let version = dictionary["CFBundleShortVersionString"] as! String
+        let build = dictionary["CFBundleVersion"] as! String
+        return "Version: \(version) (\(build))"
     }
     
     @IBAction func showSavedInFinder(_ sender: NSButton) {
