bc1d480778277e8ac98bf4bebf24b1cb825e1ee3
Иван Чистяков <efh996@gmail.com>
2017-01-08 18:07:32 +0500

    "Show in Finder" button added (doesn't work yet).

---
+++ WallpaperMaster/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WallpaperMaster
+//
+//  Created by Ivan Chistyakov on 08.01.17.
+//  Copyright © 2017 Ivan Chistyakov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    @IBAction func showSavedInFinder(_ sender: NSButton) {
+        print("Show!")
+    }
+    
+}
