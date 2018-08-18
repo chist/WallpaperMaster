b4122f2dd901f08ce8fd73174f4fb8db87469a18
Ivan Chistyakov <ivanchistyakov@mba.local>
2018-08-19 01:21:13 +0300

    Bing search is fixed.

--- WallpaperMaster/BingCollection.swift
+++ WallpaperMaster/BingCollection.swift
@@ -10,9 +10,9 @@ import Foundation
 import Cocoa
 
 class BingCollection: ImageGetterDelegate {
-    private let contentURL           = "https://www.iorise.com/"
+    private let contentURL           = "https://bingwallpaper.com/"
     private let downloader           = Downloader()
-    private let maxFailureCount: Int = 2
+    private let maxFailureCount: Int = 5
     
     private func str2(_ num: Int) -> String {
         return String(format: "%02d", num)
@@ -24,12 +24,12 @@ class BingCollection: ImageGetterDelegate {
         while triesCount < maxFailureCount {
             let date: Day
             if random {
-                let firstDay = Day(1, ofMonth: 1, inYear: 2015)
+                let firstDay = Day(8, ofMonth: 6, inYear: 2018)
                 date = DateGenerator.getRandomDay(after: firstDay)
             } else {
                 date = DateGenerator.getPreceding(by: triesCount)
             }
-            let monthLink = contentURL + "?m=\(date.year)" + str2(date.month) + str2(date.day)
+            let monthLink = contentURL + "\(date.year)" + str2(date.month) + str2(date.day) + ".html"
             
             triesCount = triesCount + 1
             
@@ -42,46 +42,31 @@ class BingCollection: ImageGetterDelegate {
             do {
                 // get some text containing image links
                 let monthHTML = try HTMLDocument(string: monthHTMLString!, encoding: String.Encoding.utf8)
-                let xpath = "/html//div[@class='entry-content']"
+                let xpath = "//*[@id=\"photos\"]/div/div[1]/img"
                 let anchorArray = monthHTML.xpath(xpath)
-                var text = ""
-                for element in anchorArray {
-                    text = text + "\n" + String(describing: element)
+                
+                if anchorArray.count == 0 {
+                    continue
                 }
                 
+                let text = String(describing: anchorArray[0])
+                
                 // get all the image links from web page
-                let ranges: [NSRange] = text.search(substring: "href=\"")
+                let ranges: [NSRange] = text.search(substring: "img src=\"//")
                 if ranges.count == 0 {
                     continue
                 }
                 
-                // store all links as [String]
-                var links = [String]()
-                for nsRange in ranges {
-                    // convert NSRange to Range
-                    let range = nsRange.range(for: text)!
-                    
-                    let index1    = range.upperBound
-                    let cutString = text[index1...]
-                    let index     = cutString.index(of: "\"")!
-                    let offset    = cutString.distance(from: cutString.startIndex, to: index)
-                    let index2    = text.index(index1, offsetBy: offset - 1)
-                    links.append(String(text[index1...index2]))
-                }
+                // extract link to the image
+                let index1    = String.Index(encodedOffset: ranges[0].upperBound)
+                let cutString = text[index1...]
+                let index     = cutString.index(of: "\"")!
+                let offset    = cutString.distance(from: cutString.startIndex, to: index)
+                let index2    = text.index(index1, offsetBy: offset - 1)
+                let link = String(text[index1...index2])
                 
-                // find link to image with desired resolution
-                let best = "1920x1080" // without logo
-                for link in links {
-                    if link.search(substring: best).count > 0 {
-                        return link
-                    }
-                }
-                let good = "1920x1200"
-                for link in links {
-                    if link.search(substring: good).count > 0 {
-                        return link
-                    }
-                }
+                return link
+
             } catch let error {
                 print(error.localizedDescription)
             }
@@ -89,19 +74,45 @@ class BingCollection: ImageGetterDelegate {
         return nil
     }
     
-    func getImageOfTheDay() -> DescribedImage {
-        let link = getLinkToImage(random: false)
+    private func getSuperResolutionLink(link: String) -> String {
+        let ranges: [NSRange] = link.search(substring: "_")
+        
+        if ranges.count == 0 {
+            return link
+        }
+        
+        // extract current resolution
+        let index1    = String.Index(encodedOffset: ranges.last!.upperBound)
+        let cutString = link[index1...]
+        let index     = cutString.index(of: ".")!
+        let offset    = cutString.distance(from: cutString.startIndex, to: index)
+        let index2    = link.index(index1, offsetBy: offset - 1)
+        let curRes = String(link[index1...index2])
+        
+        let newLink = link.replacingOccurrences(of: curRes, with: "1920x1200", options: .literal, range: nil)
+        return newLink
+    }
+    
+    private func getImage(link: String?) -> DescribedImage {
         if link != nil {
-            return downloader.getImage(from: link!)
+            let correctedLink = "http://" + link!
+//            let superResLink = getSuperResolutionLink(link: correctedLink)
+//            let superImg = downloader.getImage(from: superResLink)
+//            if superImg.image != nil {
+//                return superImg
+//            }
+            return downloader.getImage(from: correctedLink)
         }
         return DescribedImage()
     }
     
+    func getImageOfTheDay() -> DescribedImage {
+        let link = getLinkToImage(random: false)
+        return getImage(link: link)
+    }
+    
     func getRandomImage() -> DescribedImage {
         let link = getLinkToImage(random: true)
-        if link != nil {
-            return downloader.getImage(from: link!)
-        }
-        return DescribedImage()
+        return getImage(link: link)
     }
 }
